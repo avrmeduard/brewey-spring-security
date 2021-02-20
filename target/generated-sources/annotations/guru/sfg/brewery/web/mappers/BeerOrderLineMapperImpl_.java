e4ef60dd207ff9548e9e90ff1dbc956ea99ef211
avrm <avrmeduard@gmail.com>
2021-02-20 16:14:28 +0200

    up

--- target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl_.java
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl_.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-18T20:53:22+0200",
+    date = "2021-02-20T16:13:08+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
