0d95f2bdce89b6b98dec6ba046f95456e5a74f63
avrm <avrmeduard@gmail.com>
2021-02-20 14:20:01 +0200

    + config

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package guru.sfg.brewery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.KeyPairGenerator;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        KeyPairGenerator kpg
+                = KeyPairGenerator
+                .getInstance("RSA");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> {
+            authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
+                    .antMatchers("/beers/find", "/beers/**").permitAll()
+                    .antMatchers(HttpMethod.GET, "/api/v1/beer/**").permitAll()
+                    ./*antMatchers*/mvcMatchers(HttpMethod.GET, "/api/v1/beerUpc/{upc}").permitAll();
+
+        })
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("spring")
+                .password("{bcrypt}$2a$10$KsYnh/zsaFNTTTfaRCjW0uExIk91acdYav.GBeYAW8j0BORVcnheO")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{sha256}694501ade53d69a9fb21caf50ed11a7c800548bc32907078bb668ba14e379bd015223ebe0768a745")
+                .roles("USER");
+
+        auth.inMemoryAuthentication().withUser("scott").password("{noop}tiger").roles("CUSTOMER");
+    }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("spring")
+//                .password("guru")
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("password")
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin,user);
+//    }
+}
