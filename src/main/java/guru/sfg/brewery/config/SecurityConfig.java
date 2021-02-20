3f2c4e1a0071f861dc377a66ea576a100fac6b48
avrm <avrmeduard@gmail.com>
2021-02-20 17:36:48 +0200

    + constructor for filter created to be added in filter chain before authentication

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,18 +1,30 @@
 package guru.sfg.brewery.config;
 
+import guru.sfg.brewery.security.RestHeaderAuthFiler;
 import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public RestHeaderAuthFiler restHeaderAuthFiler(AuthenticationManager authenticationManager) {
+        RestHeaderAuthFiler filer = new RestHeaderAuthFiler(new AntPathRequestMatcher("/api/**"));
+        filer.setAuthenticationManager(authenticationManager);
+
+        return filer;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -21,6 +33,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(restHeaderAuthFiler(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
+
         http.authorizeRequests(authorize -> {
             authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll()
                     .antMatchers("/beers/find", "/beers/**").permitAll()
