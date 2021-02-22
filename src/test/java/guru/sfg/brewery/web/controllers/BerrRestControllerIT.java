3f1b004fe02476f5c3d3be5401b1d0ed54538da5
avrm <avrmeduard@gmail.com>
2021-02-22 21:02:33 +0200

    +

--- src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
@@ -3,6 +3,7 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,13 +20,26 @@ public class BerrRestControllerIT extends BaseIT{
     }
 
     @Test
+    void deleteBeerHttpBasic() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .with(httpBasic("spring", "guru")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void deleteBeerNoAuth() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
     void findBeer() throws Exception {
         mockMvc.perform(get("/api/v1/beer"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void findBerrById() throws Exception {
+    void findBeerById() throws Exception {
         mockMvc.perform(get("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311"))
                 .andExpect(status().isOk());
     }
