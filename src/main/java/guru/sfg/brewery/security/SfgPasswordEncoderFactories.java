12fdd4a9f2b79b97ed3599c3266b9a03769c22a0
avrm <avrmeduard@gmail.com>
2021-02-22 21:04:20 +0200

    + password encoder overridden by our needs

--- src/main/java/guru/sfg/brewery/security/SfgPasswordEncoderFactories.java
+++ src/main/java/guru/sfg/brewery/security/SfgPasswordEncoderFactories.java
@@ -1,9 +1,7 @@
 package guru.sfg.brewery.security;
 
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.*;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,9 +9,10 @@ import java.util.Map;
 public class SfgPasswordEncoderFactories {
 
     public static PasswordEncoder createDelegatingPasswordEncoder() {
-        String encodingId = "bcrypt";
+        String encodingId = "bcrypt10";
         Map<String, PasswordEncoder> encoders = new HashMap();
-        encoders.put(encodingId, new BCryptPasswordEncoder());
+        encoders.put(encodingId, new BCryptPasswordEncoder(10));
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
         encoders.put("ldap", new LdapShaPasswordEncoder());
         encoders.put("noop", NoOpPasswordEncoder.getInstance());
         encoders.put("sha256", new StandardPasswordEncoder());
