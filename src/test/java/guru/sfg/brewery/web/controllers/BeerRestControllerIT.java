97dcf69f2b769f77555d4fa4ca42002b77c43d0b
avrm <avrmeduard@gmail.com>
2021-02-23 20:27:24 +0200

    class renamed

--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 
 @WebMvcTest
-public class BerrRestControllerIT extends BaseIT{
+public class BeerRestControllerIT extends BaseIT{
 
     @Test
     void deleteBadCredential() throws Exception {
