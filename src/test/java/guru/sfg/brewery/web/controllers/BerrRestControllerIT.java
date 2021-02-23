74f954f09909588dc1a936e6e47b1036879df010
avrm <avrmeduard@gmail.com>
2021-02-23 18:55:34 +0200

    + failure test for wrong password

--- src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BerrRestControllerIT.java
@@ -5,12 +5,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 @WebMvcTest
 public class BerrRestControllerIT extends BaseIT{
+
+    @Test
+    void deleteBadCredential() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/97df0c39-90c4-4ae0-b663-453e8e19c311")
+        .header("Api-Key", "spring").header("Api-Secret", "guruWRONG"))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void deleteBeer() throws Exception{
