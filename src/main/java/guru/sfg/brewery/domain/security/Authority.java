f34f8c69617cae24bbedc7c051c92e8e84d1a7e0
avrm <avrmeduard@gmail.com>
2021-02-24 22:59:21 +0200

    + authority model and mapped to db (a copy of SimpleGrantedAuthority)

---
+++ src/main/java/guru/sfg/brewery/domain/security/Authority.java
@@ -0,0 +1,19 @@
+package guru.sfg.brewery.domain.security;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Entity
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String role;
+
+    @ManyToMany(mappedBy = "authorities")
+    @JoinColumn
+    private Set<User> users;
+}
